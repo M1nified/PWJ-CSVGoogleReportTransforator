ffe4bea8eba5a7063ec21a5969ec54b7e1e43786
m1nified <m1nified.dev@gmail.com>
2018-07-19 19:55:20 +0200

    init; basic transformation on CSV report; column selection; CANCELED removal

---
+++ CSVFixer/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿using DataAccess;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CSVFixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_clearfilelist = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button_doit = new System.Windows.Forms.Button();
+            this.checkBox_removeCanceled = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(640, 82);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(652, 144);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Files";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button_clearfilelist);
+            this.panel1.Location = new System.Drawing.Point(0, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 32);
+            this.panel1.TabIndex = 1;
+            // 
+            // button_clearfilelist
+            // 
+            this.button_clearfilelist.Location = new System.Drawing.Point(6, 3);
+            this.button_clearfilelist.Name = "button_clearfilelist";
+            this.button_clearfilelist.Size = new System.Drawing.Size(75, 23);
+            this.button_clearfilelist.TabIndex = 0;
+            this.button_clearfilelist.Text = "Clear file list";
+            this.button_clearfilelist.UseVisualStyleBackColor = true;
+            this.button_clearfilelist.Click += new System.EventHandler(this.button_clearfilelist_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.checkBox_removeCanceled);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Controls.Add(this.button_doit);
+            this.groupBox2.Location = new System.Drawing.Point(12, 162);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(652, 354);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transformation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Columns";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 32);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(263, 304);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // button_doit
+            // 
+            this.button_doit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_doit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button_doit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_doit.Location = new System.Drawing.Point(571, 325);
+            this.button_doit.Name = "button_doit";
+            this.button_doit.Size = new System.Drawing.Size(75, 23);
+            this.button_doit.TabIndex = 0;
+            this.button_doit.Text = "DO IT!";
+            this.button_doit.UseVisualStyleBackColor = false;
+            this.button_doit.Click += new System.EventHandler(this.button_doit_Click);
+            // 
+            // checkBox_removeCanceled
+            // 
+            this.checkBox_removeCanceled.AutoSize = true;
+            this.checkBox_removeCanceled.Checked = true;
+            this.checkBox_removeCanceled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_removeCanceled.Location = new System.Drawing.Point(276, 32);
+            this.checkBox_removeCanceled.Name = "checkBox_removeCanceled";
+            this.checkBox_removeCanceled.Size = new System.Drawing.Size(186, 17);
+            this.checkBox_removeCanceled.TabIndex = 3;
+            this.checkBox_removeCanceled.Text = "Remove CANCELED transactions";
+            this.checkBox_removeCanceled.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(676, 528);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PWJ CSV Manager";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FileList_OnDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private GroupBox groupBox1;
+        private Panel panel1;
+        private Button button_clearfilelist;
+        private GroupBox groupBox2;
+        private Label label1;
+        private CheckedListBox checkedListBox1;
+        private Button button_doit;
+        private CheckBox checkBox_removeCanceled;
+    }
+}
+
---
+++ CSVFixer/Form1.cs
@@ -0,0 +1,131 @@
+﻿using DataAccess;
+using Jitbit.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVFixer
+{
+    public partial class Form1 : Form
+    {
+        private System.Collections.ArrayList defaultCoumns = new System.Collections.ArrayList() {
+            "Order Creation Date",
+            "Currency of Transaction",
+            "Order Amount",
+            "Amount Charged",
+            "Financial Status",
+            "Total Tax",
+            "Buyer State",
+            "Buyer Postal Code",
+            "Buyer Country",
+            "Item 1 Name",
+            "Item 1 Price",
+            "Item 1 Quantity"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("DragEnter!");
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void FileList_OnDrop(object sender, DragEventArgs e)
+        {
+            var dropped = ((string[])e.Data.GetData(DataFormats.FileDrop));
+            var files = dropped.ToList();
+
+            if (!files.Any())
+                return;
+
+            foreach (string drop in dropped)
+                if (System.IO.Directory.Exists(drop))
+                    files.AddRange(Directory.GetFiles(drop, "*.csv", System.IO.SearchOption.AllDirectories));
+
+            foreach (string file in files)
+            {
+                if (!listBox1.Items.Contains(file) && file.ToLower().EndsWith(".csv"))
+                    listBox1.Items.Add(file);
+            }
+            AnalyzeFiles();
+            AutoCheckColumns();
+        }
+
+        private void AnalyzeFiles()
+        {
+            foreach (string file in listBox1.Items)
+            {
+                DataTable dt = DataTable.New.ReadCsv(file);
+                foreach (var columnName in dt.ColumnNames)
+                {
+                    if (!checkedListBox1.Items.Contains(columnName))
+                    {
+                        checkedListBox1.Items.Add(columnName);
+                    }
+
+                }
+            }
+        }
+
+        private void AutoCheckColumns()
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                var col = checkedListBox1.Items[i];
+                if (defaultCoumns.Contains(col))
+                {
+                    checkedListBox1.SetItemCheckState(i, CheckState.Checked);
+                }
+            }
+        }
+
+        private void TransformAndSave()
+        {
+            var fixedFileNames = new LinkedList<string>();
+            foreach (string file in listBox1.Items)
+            {
+                DataTable dt = DataTable.New.ReadCsv(file);
+                var csvExport = new CsvExport(";", false);
+                foreach(var row in dt.Rows)
+                {
+                    if(checkBox_removeCanceled.Checked && row["Financial Status"].ToLower().Trim() == "canceled")
+                    {
+                        continue;
+                    }
+
+                    csvExport.AddRow();
+                    foreach(var col in checkedListBox1.CheckedItems)
+                    {
+                        var colS = col.ToString();
+                        csvExport[colS] = row[colS];
+                    }
+                }
+                var newFileName = file + " FIXED.csv";
+                csvExport.ExportToFile(newFileName);
+                fixedFileNames.AddLast(newFileName);
+            }
+            MessageBox.Show("Saved files as:\n" + fixedFileNames.Aggregate<string>((list, item) => item + "\n"));
+        }
+
+        private void button_clearfilelist_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            checkedListBox1.Items.Clear();
+        }
+
+        private void button_doit_Click(object sender, EventArgs e)
+        {
+            TransformAndSave();
+        }
+    }
+}
